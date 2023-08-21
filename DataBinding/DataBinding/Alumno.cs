0ac8a8db7f703e96360ac6c0b41bb49df6814edc
MaycolChinito <barreramaycol028@gmail.com>
2023-08-20 21:29:52 -0600

    Agregar archivos de proyecto.

---
+++ DataBinding/DataBinding/Alumno.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace DataBinding
+{
+    public class Alumno : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        string nombre = string.Empty;
+
+        public string Nombre
+        {
+            get => nombre;
+            set
+            {
+
+                if (nombre == value)
+                
+                    return;
+                    nombre = value;
+                    onPropertyChanged(nameof(Nombre));
+                    onPropertyChanged(nameof(MostrarNombre));
+
+                
+            }
+
+        }
+            public string MostrarNombre => $"Nombre ingrasado:{Nombre}";
+
+        void onPropertyChanged(string nombre)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nombre));
+        }
+    }
+
+  }
+
